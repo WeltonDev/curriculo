b0d18dcc218bfda0a4458dc60cf1e18200a236a6
WeltonDev <welton.developer95@gmail.com>
2022-03-06 20:59:11 -0300

    curriculo março 2022

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
 dark = document.querySelector('#dark');  //Botao ativa noturno
 
-
 dark.addEventListener('click', (e) => {
     e.preventDefault(); //previne o padrão de levar para outra página
 
-    background = document.querySelector('#background')
-    leftInfos = document.querySelector('#left-container');
-    rightInfos = document.querySelector('#right-container');
+    background = document.querySelector('#background'); // seleciona o background
+    leftInfos = document.querySelector('#left-container'); // seleciona o lado esquerdo
+    rightInfos = document.querySelector('#right-container'); // seleciona o lado direito
 
-    background.classList.toggle('dark-background-container');
-    leftInfos.classList.toggle('dark-left-container');
-    rightInfos.classList.toggle('dark-right-container');
+    background.classList.toggle('dark-background-container'); // muda o background para dark mode
+    leftInfos.classList.toggle('dark-left-container'); // muda o lado esquerdo para dark mode
+    rightInfos.classList.toggle('dark-right-container'); // muda o lado direito para dark mode
+});
 
-
-
-})
